9efeedf88393a0bc4189dc837f64a3932e05feb4
Javier Diego <javier.d.fernandez7@gmail.com>
2017-06-30 12:02:53 -0500

    Presenta correctamente el Dialog con la fecha y actualiza adecuadamente el modelo basado en este

--- app/src/main/java/com/cursoslicad/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/cursoslicad/android/criminalintent/CrimeFragment.java
@@ -1,8 +1,11 @@
 package com.cursoslicad.android.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -14,6 +17,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -23,6 +27,9 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment{
     private static final String ARG_CRIME_ID = "crime_id";
     public static final String TAG = "CrimeFragment";
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -76,8 +83,21 @@ public class CrimeFragment extends Fragment{
         );
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        updateDate();
+        mDateButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        FragmentManager manager = getFragmentManager();
+                        // Le pasamos la fecha actual al DatePickerFragment
+                        DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                        // Le indicamos al datePickerFragment que el resultado se debe de mandar a
+                        // CrimeFragment
+                        dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                        dialog.show(manager, DIALOG_DATE);
+                    }
+                }
+        );
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -92,6 +112,22 @@ public class CrimeFragment extends Fragment{
         );
 
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            updateDate();
+        }
+    }
+
+    private void updateDate() {
+        mDateButton.setText(mCrime.getDate().toString());
     }
 
 }
--- app/src/main/java/com/cursoslicad/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cursoslicad/android/criminalintent/CrimeListFragment.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
